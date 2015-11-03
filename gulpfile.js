d12dd77f2e4b35adbc5c95b00c1560c465d06f43
Adam King <rak@linux.vnet.ibm.com>
2015-11-03 16:49:30 -0500

    Revert "Lint"

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,43 @@
 var gulp = require('gulp');
 
-// These paths need to be changed based on the location of the respective files on your application
+//These paths need to be changed based on the location of the respective files on your application
 var paths = {
   html:['views/**/*.html'],
   css:['public/**/*.css'],
-  js:['app.js','public/**/*.js','routes/**/*.js'],
+  js:['public/**/*.js','routes/**/*.js','app.js'],
 };
-
-gulp.task('default', function(callback) {
-  var runSequence=require('run-sequence');
-  runSequence('lint', 'dev-unit', callback);
-  });
-
+var runSequence = require('run-sequence');
+var karma = require('karma').server;
+var mocha = require('gulp-mocha');
+var bower = require('gulp-bower');
+var shell = require('gulp-shell');
 
 /*
- Gulp tasks for linting
- */
-gulp.task('lint', ['lint-js','lint-css', 'lint-jscs']);
+Gulp tasks for linting
+*/
 
-// Use jshint to check Javascript syntax
-gulp.task('lint-js', function() {
-    var jshint = require('gulp-jshint');
-    var mkdirp = require('mkdirp');
-    mkdirp.sync('./reports');
-    return gulp.src(paths.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('jshint-junit-reporter',{ outputFile: "./reports/jshint.xml"}))
-    .pipe(jshint.reporter('fail'));
+gulp.task('default', function(callback) {
+  runSequence('lint', 'dev-unit', callback);
 });
 
+gulp.task('lint-js', shell.task(
+    ['node_modules/.bin/jshint app.js routes/ --reporter=node_modules/jshint-junit-reporter/reporter.js > test/jslint.xml'],
+    { cwd: __dirname, ignoreErrors: false }
+));
 
-// Use jscs to check for code style compliance
-gulp.task('lint-jscs', function() {
-    var jscs = require('gulp-jscs-with-reporter');
-    var jscsJenkinsReporter = require('gulp-jenkins-reporter');
-    var logCapture = require('gulp-log-capture');
-    return gulp.src(paths.js)
-    .pipe(jscs({
-              "preset": "node-style-guide",
-              "requireCapitalizedComments": { "allExcept":["jslint", "jshint"]},
-              "requireCurlyBraces": null
-          }))
-    .pipe(logCapture.start(console, 'log'))
-    .pipe(jscs.reporter(jscsJenkinsReporter))
-    .pipe(logCapture.stop('xml'))
-    .pipe(jscs.reporter('fail'))
-    .pipe(gulp.dest('./reports'))
-    ;
-});
+gulp.task('lint-css', shell.task(
+    ['node_modules/.bin/csslint public/**/*.css --ignore=box-model,ids --format=junit-xml > test/csslint.xml'],
+    { cwd: __dirname, ignoreErrors: false }
+));
 
-// Use csslint to check for CSS issues
-gulp.task('lint-css', function() {
-  var csslint=require('gulp-csslint');
-  return gulp.src(paths.css)
-    .pipe(csslint({
-          "ids":false,
-          "adjoining-classes": false,
-          "box-sizing": false,
-          "box-model": false,
-          "compatible-vendor-prefixes": false,
-          "font-sizes": false,
-          "gradients": false,
-          "important": false,
-          "known-properties": false,
-          "qualified-headings": false,
-          "regex-selectors": false,
-          "shorthand": false,
-          "text-indent": false,
-          "unique-headings": false,
-          "universal-selector": false,
-          "unqualified-attributes": false,
-          "vendor-prefix": false,
-          "overqualified-elements": false,
-          "star-property-hack": false,
-          "underscore-property-hack": false
-        }))
-    .pipe(csslint.reporter())
-    .pipe(csslint.reporter('fail'))
-    ;
-  });
-
+gulp.task('lint', ['lint-js','lint-css']);
 
 /*
 Gulp tasks for unit tests
 */
-gulp.task('dev-unit', ['dev-karma','dev-mocha']);
 
 //Task for karma (frontend) unit tests
 gulp.task('dev-karma', function(done) {
-  var karma = require('karma').server;
   karma.start({
     configFile: __dirname + '/test/karma.conf.js',
     singleRun: true,
@@ -98,7 +46,6 @@ gulp.task('dev-karma', function(done) {
 
 //Task for mocha (server) unit tests
 gulp.task('dev-mocha', function() {
-  var mocha = require('gulp-mocha');
   return gulp.src('test/unit/server/**/*spec.js', {read: false})
     .pipe(mocha({
       globals:['expect'],
@@ -109,13 +56,14 @@ gulp.task('dev-mocha', function() {
       reporter: 'mocha-jenkins-reporter',
       reporterOptions: {
         junit_report_name: 'Mocha Unit Tests for Server',
-        junit_report_path: './reports/mocha-report.xml',
+        junit_report_path: 'test/mocha-report.xml',
         junit_report_stack: 1,
       },
     }));
 });
 
 gulp.task('dev-setup', function() {
-  var bower = require('gulp-bower');
   return bower();
 });
+
+gulp.task('dev-unit', ['dev-karma','dev-mocha']);
